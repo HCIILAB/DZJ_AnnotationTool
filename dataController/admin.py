91b0d95c07b1a9567a15038ab48aed419d0c8ea4
Kelvin <515771237@qq.com>
2017-09-05 16:59:43 +0800

    Activating Admin Page

--- dataController/admin.py
+++ dataController/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import CompleteDatas, LoginDatas, MessageDatas
 
 # Register your models here.
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('PhotoPath', 'Email', 'Status',)
+    search_fields = ('PhotoPath','Email',)
+    list_filter = ('Status',)
+
+admin.site.register(CompleteDatas)
+admin.site.register(MessageDatas, MessageAdmin)
+admin.site.register(LoginDatas)
