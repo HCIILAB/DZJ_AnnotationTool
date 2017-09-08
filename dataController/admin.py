81dd723b77f4a10f4def6d334e164deb5ce89628
Kelvin <515771237@qq.com>
2017-09-08 10:50:27 +0800

    Develop admin page: 1.reset message data from data 2.reset data from user 3.redistibute data to user 4.filtering message data with status and searching with PhotoPath and Email 5.searching user with CurrentImg

--- dataController/admin.py
+++ dataController/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import CompleteDatas, LoginDatas, MessageDatas
+from .tools import getNextPhoto
 
 # Register your models here.
 
@@ -12,13 +13,38 @@ class MessageAdmin(admin.ModelAdmin):
 
     def resetToNotDistributed(self, request, queryset):
         queryset.update(Status='00')
+        for obj in queryset:
+            cmpltData = CompleteDatas.objects.get(CurrentImg=obj.PhotoPath)
+            if cmpltData:
+                cmpltData.CurrentImg = '-1'
+                cmpltData.save()
     resetToNotDistributed.short_description = "Reset selected photos as not distributed"
 
 
 class CompleteDatasAdmin(admin.ModelAdmin):
     """docstring for CompleteDatasAdmin"""
     list_display = ('Email', 'Count', 'CurrentImg',)
-    search_fields = ('Email',)
+    search_fields = ('Email', 'CurrentImg',)
+    actions = ['retrieveDataFromUser',
+               'distribNewDataToUser', ]
+
+    def retrieveDataFromUser(self, request, queryset):
+        for obj in queryset:
+            md = MessageDatas.objects.get(PhotoPath=obj.CurrentImg)
+            if md:
+                md.Status = '00'
+                md.save()
+            obj.CurrentImg = '-1'
+            obj.save()
+        # queryset.update(CurrentImg='-1')
+    retrieveDataFromUser.short_description = "Retrieve data from selected user"
+
+    def distribNewDataToUser(self, request, queryset):
+        # queryset.update(CurrentImg=getNextPhoto(' '))
+        for obj in queryset:
+            obj.CurrentImg = getNextPhoto('')
+            obj.save()
+    distribNewDataToUser.short_description = "Distribute new data to selected user"
 
 
 admin.site.register(CompleteDatas, CompleteDatasAdmin)
